d2999bbb19eb9c1095f52c00159d842485c7708c
johnjack2007 <jack@iqid.io>
2025-11-29 11:14:40 -0600

    added the correct dimensions for the grid.  Added input buttons for user to customize.  Added the hover effect but it is transitional only and not permanent

---
+++ function.js
@@ -0,0 +1,49 @@
+const container = document.querySelector(".container");
+
+const square1 = document.createElement("div");
+container.appendChild(square1);
+
+const square2 = document.createElement("div");
+container.appendChild(square2);
+
+const square3 = document.createElement("div");
+container.appendChild(square3);
+
+const square4 = document.createElement("div");
+container.appendChild(square4);
+
+const square5 = document.createElement("div");
+container.appendChild(square5);
+
+const square6 = document.createElement("div");
+container.appendChild(square6);
+
+const square7 = document.createElement("div");
+container.appendChild(square7);
+
+const square8 = document.createElement("div");
+container.appendChild(square8);
+
+const square9 = document.createElement("div");
+container.appendChild(square9);
+
+const square10 = document.createElement("div");
+container.appendChild(square10);
+
+const square11 = document.createElement("div");
+container.appendChild(square11);
+
+const square12 = document.createElement("div");
+container.appendChild(square12);
+
+const square13 = document.createElement("div");
+container.appendChild(square13);
+
+const square14 = document.createElement("div");
+container.appendChild(square14);
+
+const square15 = document.createElement("div");
+container.appendChild(square15);
+
+const square16 = document.createElement("div");
+container.appendChild(square16);
