c06b830c8170d512fb7b4f870cea931f61decbb8
johnjack2007 <jack@iqid.io>
2025-11-29 22:56:52 -0600

    button logic updated

--- function.js
+++ function.js
@@ -4,3 +4,15 @@ const container = document.querySelector(".container");
 //2. I need to create an event listener for the submit button and have it have a function to append the number of divs to it as the input number.
 //3. I need to add the hover effect so that it puts the color over each square and make it stay.
 //4. I need to add functionality to have it reset on the ssubmit button or maybe not
+
+const input = document.querySelector(".input");
+const submit = document.querySelector(".submit");
+
+submit.addEventListener("click", ()=> {
+    const inputValue = input.value;
+
+    for (let i = 0; i < inputValue; i++){
+        
+    }
+
+});
