661b1995e9d47a8953093a20eefa64b12f64eb7d
johnjack2007 <jack@iqid.io>
2025-11-29 21:42:10 -0600

    planned out the rest of the architecture

--- function.js
+++ function.js
@@ -1,49 +1,6 @@
 const container = document.querySelector(".container");
 
-const square1 = document.createElement("div");
-container.appendChild(square1);
-
-const square2 = document.createElement("div");
-container.appendChild(square2);
-
-const square3 = document.createElement("div");
-container.appendChild(square3);
-
-const square4 = document.createElement("div");
-container.appendChild(square4);
-
-const square5 = document.createElement("div");
-container.appendChild(square5);
-
-const square6 = document.createElement("div");
-container.appendChild(square6);
-
-const square7 = document.createElement("div");
-container.appendChild(square7);
-
-const square8 = document.createElement("div");
-container.appendChild(square8);
-
-const square9 = document.createElement("div");
-container.appendChild(square9);
-
-const square10 = document.createElement("div");
-container.appendChild(square10);
-
-const square11 = document.createElement("div");
-container.appendChild(square11);
-
-const square12 = document.createElement("div");
-container.appendChild(square12);
-
-const square13 = document.createElement("div");
-container.appendChild(square13);
-
-const square14 = document.createElement("div");
-container.appendChild(square14);
-
-const square15 = document.createElement("div");
-container.appendChild(square15);
-
-const square16 = document.createElement("div");
-container.appendChild(square16);
+//I need to create the submit button and input as js variables.
+//2. I need to create an event listener for the submit button and have it have a function to append the number of divs to it as the input number.
+//3. I need to add the hover effect so that it puts the color over each square and make it stay.
+//4. I need to add functionality to have it reset on the ssubmit button or maybe not
